48433be29cfd1c2e8dfd16aa6d1850ee6f5cc1d1
Elderl23 <lopez_felix_23@hotmail.com>
2015-03-10 16:16:29 -0600

    union de frontend y backend y reajuste del sistema del template

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, render_to_response, get_object_or_404,render
 from django.template import RequestContext
 
+from categorias.models import Categoria
+
 # Create your views here.
 def vw_inicio(request):
-	return render_to_response('index.html', context_instance=RequestContext(request))
+	ObjCategoria = Categoria.objects.all()
+	ctx={
+        'Categoria':ObjCategoria,
+    }
+	return render_to_response('index.html',ctx, context_instance=RequestContext(request))
 
 
 def vw_prueba(request):
