a90198cba70ba49a69c27302ef2d8bb77ea5d471
Elderl23 <lopez_felix_23@hotmail.com>
2015-03-13 11:54:49 -0600

    correcion del modulo post a etiquetas

--- post/models.py
+++ post/models.py
@@ -8,7 +8,8 @@ from etiquetas.models import Etiqueta
 class Post(models.Model):
 	title = models.CharField(max_length=255)
 	order = models.PositiveIntegerField()
-	etiquetas = models.ForeignKey(Etiqueta)
+	#etiquetas = models.ForeignKey(Etiqueta)
+	etiquetas = models.ManyToManyField(Etiqueta, blank=True, null=True)
 	
 
 	def __unicode__(self):
