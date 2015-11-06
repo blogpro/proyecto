ba3f146cd2db14b7b0c8c5239faf249dc4f08431
Elder Lopez Felix <lopez_felix_23@hotmail.com>
2015-11-06 15:27:04 -0600

    Se agrego el arcchivo google-site-verification

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -4,10 +4,16 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'proyecto.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    #----------------google--------------------
+    (r'^google0aaeb5398aea27d9\.html$', lambda r: HttpResponse("google-site-verification: google0aaeb5398aea27d9.html")),
+    #----------------google--------------------
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^',include('inicio.urls')),
