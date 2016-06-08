3dd6c38432a1fcac21155d6e7e2af41bcfb9b8b7
Elder <lopez_felix_23@hotmail.com>
2016-06-08 11:15:30 -0500

    Se agrego el template para el dash y se hizo la primera prueba

--- post/views.py
+++ post/views.py
@@ -31,7 +31,7 @@ class LoginRequiredMixin(object):
 
 # Create your views here.
 class PostView(LoginRequiredMixin,FormView):
-    template_name = 'post.html'
+    template_name = 'baseDash.html'
     form_class = PostForm
     #success_url = '/add-item-post/'
 
