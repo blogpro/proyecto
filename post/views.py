90809f56dbbc56454f749cc5bdd66f7fd7393b29
Elder <lopez_felix_23@hotmail.com>
2016-06-14 17:20:20 -0500

    Actualizacion del commit anterior error

--- post/views.py
+++ post/views.py
@@ -195,41 +195,6 @@ class DeleteItemPostView(RedirectView):
 
 		return HttpResponseRedirect(url+str(positem_id)+"/")
 
-class PostViewp(LoginRequiredMixin,FormView):
-    template_name = 'post.html'
-    form_class = PostForm
-    success_url = '/post/'
-
-    def form_valid(self, form):
-        title = form.cleaned_data['title']
-        form.save()
-        return self.render_to_response(self.get_context_data(form=form))
-    def form_invalid(self, form):
-    	return self.render_to_response(self.get_context_data(form=form))    
-    
-    
-
-    #Retorna los valores al template como nuevas variables
-    def get_context_data(self, **kwargs):
-		context = super(PostView, self).get_context_data(**kwargs)
-		ObjCategoria = Categoria.objects.all()
-		ObjEtiqueta = Etiqueta.objects.all()
-		ObjMenu = Menu.objects.all()
-		ObjPost = Post.objects.all()
-
-		ObjFormPost = PostForm()
-		#Mandamos a llamar al formulario del Post
-
-		data = {
-			'Categoria':ObjCategoria,
-			'Etiqueta':ObjEtiqueta,
-			'Menu':ObjMenu,
-			'ObjFormPost':ObjFormPost,
-			'Post':ObjPost,
-		}
-
-		context.update(data)
-		return context
 class ListPostViewPost(MenuMixin,AsideMixin,GetPostSlugMixin, TemplateView):
 	model = Post
 	template_name = 'dashlistpost.html'
