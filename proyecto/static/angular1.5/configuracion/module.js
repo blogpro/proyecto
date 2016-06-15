4adcb5d009c4f503e2d99a77bde5da7be7c3a06e
Elder <lopez_felix_23@hotmail.com>
2016-06-15 13:23:18 -0500

    Configuracion de el modulo lista de post para que se realice con angular

--- proyecto/static/angular1.5/configuracion/module.js
+++ proyecto/static/angular1.5/configuracion/module.js
@@ -7,10 +7,14 @@ var angularRoutingApp = angular.module('blog', [
 angularRoutingApp.config(function($routeProvider) {
 
     $routeProvider
+        .when('/', {
+            templateUrl : '/index-list-post-anglar/',
+            controller  : 'indexController'
+        }).
         .when('/acerca', {
             templateUrl : '/acerca/',
             controller  : 'indexController'
-        })
+        }).
         .otherwise({
             redirectTo: '/'
         });
