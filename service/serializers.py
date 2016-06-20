32ec3a001c9c5d4e840c0fa70d4d42426e27da31
Elder <lopez_felix_23@hotmail.com>
2016-06-20 18:02:24 -0500

    Prueba de union del servicio con rest2

--- service/serializers.py
+++ service/serializers.py
@@ -2,9 +2,17 @@ from django.db.models.base import Model
 from django.forms import widgets
 from rest_framework import serializers
 
-from post.models import Post
+from django.contrib.auth.models import User
+
+from post.models import Post, Status_Post
+
+class UserPerSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('first_name',)
 
 class PostQuerySerializer(serializers.ModelSerializer):
+	user = UserPerSerializer()
 	class Meta:
 		model = Post
 		fields = ('id','title','status','user')
