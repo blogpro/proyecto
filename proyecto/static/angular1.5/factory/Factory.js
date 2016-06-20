ea16e667c4c7dd06ad7d412d1082d52e7f22d904
Elder <lopez_felix_23@hotmail.com>
2016-06-20 12:45:50 -0500

    Prueba de union del servicio con rest2

--- proyecto/static/angular1.5/factory/Factory.js
+++ proyecto/static/angular1.5/factory/Factory.js
@@ -3,9 +3,8 @@ angular.module('factoryModule', [])
 	.factory('ServiceHTTP', ['$resource', '$rootScope','$http',function ($resource, $rootScope, $http) {
         return {
             post: function (datos, servicio) {
-                $http.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8';
                 var urlApi = "http://systab.herokuapp.com/";
-                	resource   = $resource(urlApi + servicio),
+                	resource   = $resource(urlApi + "service-post-query/"),
 
                 console.log("dato1 "+JSON.stringify(resource));
                 return resultado;
