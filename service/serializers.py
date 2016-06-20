106ef2e771959ee89dbcc48a8d5f96396a45b95f
Elder <lopez_felix_23@hotmail.com>
2016-06-20 10:39:43 -0500

    Actualizacion servicio para retornal la lista de post prueba5

---
+++ service/serializers.py
@@ -0,0 +1,10 @@
+from django.db.models.base import Model
+from django.forms import widgets
+from rest_framework import serializers
+
+from post.models import Post
+
+class PostQuerySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ('id','title',)
