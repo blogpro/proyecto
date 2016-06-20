a758514dbcc5370486198b728ae9027b0d08d72b
Elder <lopez_felix_23@hotmail.com>
2016-06-20 11:09:38 -0500

    Creacion del servicio para obtener la lista de los post

--- service/urls.py
+++ service/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .views import ServicePostQuery
 
 urlpatterns = patterns('',
-   url(r'^servie-post-query/$', ServicePostQuery.as_view()),
+   url(r'^service-post-query/$', ServicePostQuery.as_view()),
    #url(r'^nota-detail/(?P<pk>[0-9]+)/$', NotaDetail.as_view()),
 
 )
