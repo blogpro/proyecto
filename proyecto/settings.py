3c6a86cd327e47289d4ce7e05d9dc2c860a6148d
Elder <lopez_felix_23@hotmail.com>
2016-06-20 11:33:39 -0500

    Permisos en los rest

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -73,6 +73,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
