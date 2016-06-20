0b68bb3f1b6ab5b5c072ed4fba16461d5b26b449
Elder <lopez_felix_23@hotmail.com>
2016-06-20 18:20:11 -0500

    Prueba de union del servicio con rest2

--- service/serializers.py
+++ service/serializers.py
@@ -10,9 +10,14 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('first_name',)
+class StatusSerializer(serializers.ModelSerializer):
+	class Meta:
+		model Status_Post
+		fields = ('title',)		
 
 class PostQuerySerializer(serializers.ModelSerializer):
 	user = UserSerializer()
+	status = StatusSerializer()
 	class Meta:
 		model = Post
 		fields = ('id','title','status','user')
