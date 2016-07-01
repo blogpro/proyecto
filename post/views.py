8381ad3ee631567f51ab5758ad002c4ab72d9d86
Elder <lopez_felix_23@hotmail.com>
2016-07-01 10:15:31 -0500

    Actulizacion para la url de etitar post

--- post/views.py
+++ post/views.py
@@ -68,6 +68,11 @@ class UpdatePostViewPost(UpdateView):
 	model = Post
 	template_name = 'dashpost.html'
 
+
+	def form_valid(self, form):
+		return HttpResponseRedirect('/list-post/')
+		#return reverse('InicioViewAdmin')
+
 	def get_context_data(self, **kwargs):
 
 		context = super(UpdatePostViewPost, self).get_context_data(**kwargs)
