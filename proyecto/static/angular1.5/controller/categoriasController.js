b979ebb918960730115dac1c85447aa1d2e9e635
Elder <lopez_felix_23@hotmail.com>
2016-07-04 10:54:03 -0500

    Actualizacion de categorias

--- proyecto/static/angular1.5/controller/categoriasController.js
+++ proyecto/static/angular1.5/controller/categoriasController.js
@@ -3,11 +3,20 @@ angular.module('categoriaModuleController', [])
 	.controller('categoriaController',['$scope', '$rootScope','ServiceHTTP',
         function($scope,$rootScope,ServiceHTTP)
         {
-        	
-    	    ServiceHTTP.query('service-categorias-query/').$promise.then(function(result) {
-			   $scope.postQuery = result;
-			}, function(errResponse) {
-			   console.log("error "+errResponse);
-			});
+
+        	$scope.consultaCategorias = function () {
+	    	    ServiceHTTP.query('service-categorias-query/').$promise.then(function(result) {
+				   $scope.postQuery = result;
+				}, function(errResponse) {
+				   console.log("error "+errResponse);
+				});
+    		}
+
+			$scope.consultaCategorias();
+
+
+
+
+
         }
     ]);        	
