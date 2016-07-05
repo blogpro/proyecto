e6ce9546ca0e266b519356b6da38e3ae02c06e4f
Elder <lopez_felix_23@hotmail.com>
2016-07-05 18:07:01 -0500

    Creacion del servicio para gardar categorias pruba1

--- proyecto/static/angular1.5/factory/Factory.js
+++ proyecto/static/angular1.5/factory/Factory.js
@@ -13,8 +13,7 @@ angular.module('factoryModule', [])
                 console.log(data);
                 var defer = $q.defer();
 
-                $http.post('http://systab.herokuapp.com/'+servicio, data, {transformRequest: angular.identity,headers: {'Content-Type': undefined}
-                }).
+                $http.post('http://systab.herokuapp.com/'+servicio, data).
                 success(function (data, status, headers, config) {
                     defer.resolve(data);
                 }).error(function (data, status, headers, config) {
