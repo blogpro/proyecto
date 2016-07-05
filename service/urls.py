272a85e6dd2e83f679ff0d91ec7e9e150f206365
Elder <lopez_felix_23@hotmail.com>
2016-07-05 10:42:28 -0500

    Se regreso a un commit anterior error no identificado

--- service/urls.py
+++ service/urls.py
@@ -7,5 +7,4 @@ urlpatterns = patterns('',
    url(r'^service-post-query/$', ServicePostQuery.as_view()),
    url(r'^service-categorias-query/$', ServiceCategoriasQuery.as_view()),
    #url(r'^nota-detail/(?P<pk>[0-9]+)/$', NotaDetail.as_view()),
-
 )
