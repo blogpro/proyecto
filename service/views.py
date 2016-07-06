2549fee727d7ce771a614a97589a900db33495c9
Elder <lopez_felix_23@hotmail.com>
2016-07-06 10:31:47 -0500

    prueba para guardar la categoria con servicio1

--- service/views.py
+++ service/views.py
@@ -44,8 +44,7 @@ class ServiceCategoriasQuery(APIView):
 			print 'ERROR:', err
 		return Response(serializer.data)
 	def post(self, request, *args, **kwargs):
-		#token = request.POST
-		token = self.kwargs['title']
+		token = request.POST
 		data = {
 			'login': token,
 			'message': "ok"
