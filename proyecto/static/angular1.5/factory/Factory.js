3d9a051884150b9e1796de21b329154eab277355
Elder <lopez_felix_23@hotmail.com>
2016-07-11 13:56:10 -0500

    Prueba 4 de resource save

--- proyecto/static/angular1.5/factory/Factory.js
+++ proyecto/static/angular1.5/factory/Factory.js
@@ -1,21 +1,23 @@
 angular.module('factoryModule', [])
 
 	.factory('ServiceHTTP', ['$resource', '$rootScope','$q','$http',function ($resource, $rootScope, $q, $http) {
-        var urlApi = "http://systab.herokuapp.com/";
-        var resultado = "";
+        var urlApi = "http://systab.herokuapp.com/",
+            resource = "",
+            resultado = "";
+
         return {
             query: function (url) {
-                var resource   = $resource(urlApi + url),
-                resultado = resource.query().$promise.then(function(result) {
+                resource   = $resource(urlApi + url);
+                resultado = resource.query();
+                
+                resultado.$promise.then(function(result) {
                    return result;
                 }, function(errResponse) {
                    console.log("error "+errResponse);
                 });
-
-                //return resultado;
             },
             post: function (url,data) {
-                var resource   = $resource(urlApi + url),
+                resource   = $resource(urlApi + url);
                 resultado = resource.save(data);   
                 return resultado;
             }
