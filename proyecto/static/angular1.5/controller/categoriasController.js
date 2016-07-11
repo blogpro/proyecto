408dd675472f776946c824119bbe9e649d6c364a
Elder <lopez_felix_23@hotmail.com>
2016-07-11 13:59:38 -0500

    Prueba 4 de resource save

--- proyecto/static/angular1.5/controller/categoriasController.js
+++ proyecto/static/angular1.5/controller/categoriasController.js
@@ -5,13 +5,18 @@ angular.module('categoriaModuleController', [])
         {
         	$scope.categorias = "";
 
-        	$scope.queryCategorias = function () {
-	    	    ServiceHTTP.query('service-categorias-query/').$promise.then(function(result) {
-				   $scope.postQuery = result;
-				}, function(errResponse) {
-				   console.log("error "+errResponse);
-				});
-    		}
+            $scope.queryCategorias = function () {
+                $scope.postQuery = ServiceHTTP.query('service-categorias-query/');
+                console.log($scope.postQuery);
+            }
+
+    //     	$scope.queryCategorias = function () {
+	   //  	    ServiceHTTP.query('service-categorias-query/').$promise.then(function(result) {
+				//    $scope.postQuery = result;
+				// }, function(errResponse) {
+				//    console.log("error "+errResponse);
+				// });
+    // 		}
 			$scope.queryCategorias();
 
 			$scope.saveCategorias = function ()
