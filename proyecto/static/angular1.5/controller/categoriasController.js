5504ea152bc6cfbc437c607c5d64d0fff3c25d70
Elder <lopez_felix_23@hotmail.com>
2016-07-13 12:58:50 -0500

    Actualizacion del serializer

--- proyecto/static/angular1.5/controller/categoriasController.js
+++ proyecto/static/angular1.5/controller/categoriasController.js
@@ -4,6 +4,7 @@ angular.module('categoriaModuleController', [])
         function($scope,$timeout,ServiceHTTP)
         {
         	$scope.categorias = "";
+            $scope.categorias.order = 0;
 
         	$scope.queryCategorias = function () {
 	    	    ServiceHTTP.query('service-categorias-query/').$promise.then(function(result) {
