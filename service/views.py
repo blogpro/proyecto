bf27d89b6a2fce06703de32ec5ca7cfec487eca8
Elder <lopez_felix_23@hotmail.com>
2016-07-13 19:14:06 -0500

    Actualizacion del serializer

--- service/views.py
+++ service/views.py
@@ -37,7 +37,7 @@ class ServicePostQuery(APIView):
 
 class ServiceCategoriasQuery(APIView):
 	def get(self, request, *args, **kwargs):
-		#pkCat = self.kwargs['pk']
+		pkCat = self.kwargs['pk']
 
 		serializers = Categoria.objects.all().order_by('-id')
 		try:
