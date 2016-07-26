4544cb9af9590c3f2b6bb240359d3a8c5a745a62
Elder <lopez_felix_23@hotmail.com>
2016-07-26 09:39:44 -0500

    prueba1

--- proyecto/static/angular1.5/controller/categoriasController.js
+++ proyecto/static/angular1.5/controller/categoriasController.js
@@ -22,10 +22,8 @@ angular.module('categoriaModuleController', [])
 			$scope.saveCategorias = function ()
         	{
                 ServiceHTTP.post('service-categorias-query/',$scope.categorias).$promise.then(function(result) {
-                    console.log(result.setCod);
                     if(result.setCod == 0){
-                        NgToast.msjToast({mensaje: 'Guardado correctamente.', clase: 'danger'});
-                        console.log("Paso a guaedar");
+                        NgToast.msjToast({mensaje: 'Guardado correctamente.', clase: 'success'});
                     }
                     $scope.categorias = "";
                     $scope.queryCategorias();
