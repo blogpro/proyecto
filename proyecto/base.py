5d593708045ebda35843713528623e9a95a9a260
“elopezf” <elopezf@elektra.com.mx>
2017-04-24 12:51:54 -0500

    uodate

--- proyecto/base.py
+++ proyecto/base.py
@@ -5,7 +5,6 @@ Produccion
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #debug = False  
-<<<<<<< HEAD
 HOSTS = ['http://systab.herokuapp.com/','systab.herokuapp.com']
 
 debug = True
@@ -23,18 +22,6 @@ PASSWORD = '3eyYWb0OMg9o5VNlkEa4QQPkpu'
 # NAME = 'bdpro'
 # USER = 'postgres'
 # PASSWORD = ''
-=======
-#HOSTS = ['http://systab.herokuapp.com/','systab.herokuapp.com']
-
-debug = True
-HOSTS = ['*']
-
-#Bd
-HOST = 'localhost'
-NAME = 'bdpro'
-USER = 'postgres'
-PASSWORD = ''
->>>>>>> origin/master
 
 facebook_key = "1539556102987271"#Produccion
 facebook_secret = "330159cf0d1b6a8a98c0e35c564365a8"#Produccion
