963f77c3d47885f5168349318a36233196fd1304
“elopezf” <elopezf@elektra.com.mx>
2017-04-24 12:44:13 -0500

    Se agrega base

---
+++ proyecto/base.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+"""
+Archivo de configuracion para 
+Produccion
+"""
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#debug = False  
+#HOSTS = ['http://systab.herokuapp.com/','systab.herokuapp.com']
+
+debug = True
+HOSTS = ['*']
+
+#Bd
+HOST = 'localhost'
+NAME = 'bdpro'
+USER = 'postgres'
+PASSWORD = ''
+
+facebook_key = "1539556102987271"#Produccion
+facebook_secret = "330159cf0d1b6a8a98c0e35c564365a8"#Produccion
+
+
