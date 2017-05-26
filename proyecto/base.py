6f9b9d94b048110170341ec6b13887e16a3464f8
“elopezf” <elopezf@elektra.com.mx>
2017-05-26 12:04:10 -0500

    actualizaciones

--- proyecto/base.py
+++ proyecto/base.py
@@ -11,16 +11,16 @@ debug = True
 # HOSTS = ['*']
 
 #Bd Produccion
-# HOST = 'ec2-54-225-201-25.compute-1.amazonaws.com'
-# NAME = 'd8aecn4b0n19m3'
-# USER = 'nnwgvgoklyndcu'
-# PASSWORD = '3eyYWb0OMg9o5VNlkEa4QQPkpu'
+HOST = 'ec2-54-225-201-25.compute-1.amazonaws.com'
+NAME = 'd8aecn4b0n19m3'
+USER = 'nnwgvgoklyndcu'
+PASSWORD = '3eyYWb0OMg9o5VNlkEa4QQPkpu'
 
 #Bd desarrollo
-HOST = 'localhost'
-NAME = 'bdpro'
-USER = 'postgres'
-PASSWORD = ''
+# HOST = 'localhost'
+# NAME = 'bdpro'
+# USER = 'postgres'
+# PASSWORD = ''
 
 facebook_key = "1476656729277209"#Produccion
 facebook_secret = "5c1f80475bcef95cef58f523ab14df27"#Produccion
